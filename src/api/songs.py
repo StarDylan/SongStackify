2a16591bbb942ebb045478499734aaa9f8069605
Dylan Starink <dylan@starink.com>
2023-10-27 13:29:35 -0700

    Add play individual song

--- src/api/songs.py
+++ src/api/songs.py
@@ -1,6 +1,8 @@
 from typing import Annotated
 from fastapi import APIRouter, Header
 from pydantic import BaseModel
+import sqlalchemy
+from src import database as db
 
 from src.api.models import SongPlayLink
 
@@ -37,5 +39,47 @@ def remove_song(song_id: int, authorization: SongAuthorization):
 @router.get("/{song_id}/play")
 def play_song(song_id: int, user_id: Annotated[str | None, Header()]) -> SongPlayLink:
     """ """
-    raise NotImplementedError()
+    with db.engine.begin() as conn:
+        query = conn.execute(sqlalchemy.text("""
+            SELECT song_url from songs
+            JOIN links ON links.song_id = songs.id
+            WHERE songs.id = :song_id 
+            AND links.platform_id = (
+                SELECT platform_id FROM users
+                WHERE users.id = :user_id
+            )
+            """),
+            [{
+                "song_id": song_id,
+                "user_id": user_id
+            }]).one_or_none()
+        
+        if query is None:
+            # Figure out what the error is
+            query = conn.execute(sqlalchemy.text("""
+                SELECT * FROM users
+                WHERE users.id = :user_id
+                """),
+                [{
+                    "user_id": user_id
+                }]).one_or_none()
+            
+            if query is None:
+                return "Invalid user ID"
+
+            query = conn.execute(sqlalchemy.text("""
+                SELECT * FROM songs
+                WHERE songs.id = :song_id
+                """),
+                [{
+                    "song_id": song_id
+                }]).one_or_none()
+                                                 
+            if query is None:
+                return "Invalid song ID"
+
+            return "Song not available on user's platform"
+        
+        return query.song_url
+
 
