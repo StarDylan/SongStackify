1db0b0efdd28b8d0a120c275e2d7b7d1c7034c83
mkw_pop <mattkadenwong@gmail.com>
2023-10-30 13:28:05 -0700

    add song to playlist

--- src/api/playlists.py
+++ src/api/playlists.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Header
 from pydantic import BaseModel
-
+from src import database as db
+import sqlalchemy
 from src.api.models import SongPlayLink
 
 router = APIRouter(
@@ -25,10 +26,19 @@ class Song(BaseModel):
 @router.post("/{playlist_id}/songs/add")
 def add_song_to_playlist(playlist_id: int, song: Song):
     """ """
-    raise NotImplementedError()
+    with db.engine.begin() as connection:
+        user_result = connection.execute(sqlalchemy.text(
+            """INSERT INTO playlist_songs(playlist_id, song_id)
+            VALUES (:playlist_id, :song_id)"""),
+        [{
+            "playlist_id":playlist_id,
+            "song_id":song
+        }]
+        )
 
 @router.get("/{playlist_id}/play")
 def play_playlist(playlist_id: int, user_id: str = Header(None)) -> SongPlayLink:
     """ """
+    
     raise NotImplementedError()
 
